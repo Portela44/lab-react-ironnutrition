9b82dea2bd612ee7331fe352e284b4ee897de005
Guillem <guillem.mart7@gmail.com>
2022-08-15 18:07:36 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import foods from './foods.json';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Food List</h1>
+      {foods.map(food => { 
+        return <div key={food.name}>
+          <p>{food.name}</p>
+          <img src={food.image} width={100} alt={food.name}/>
+      </div>
+      })}
     </div>
   );
 }
